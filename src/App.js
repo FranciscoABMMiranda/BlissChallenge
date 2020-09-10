3107b4070f516c232169dc23ff97c85eb86b976e
FranciscoABMMiranda <franciscoabmmiranda@gmail.com>
2020-09-10 15:32:02 +0100

    Loading screen waits for connection. Retry widget created for connection failure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,47 @@
 import React from "react";
 import "./App.css";
 import { LoadingScreen } from "./components/LoadingScreen";
+import { RetryWidget } from "./components/shared";
 
 class App extends React.Component {
   state = {
     serverStatus: false,
+    statusFailed: false,
   };
 
+  componentDidMount() {
+    this.getServerStatus();
+  }
+
+  getServerStatus() {
+    const url =
+      "https://private-anon-7eb2956589-blissrecruitmentapi.apiary-mock.com/health";
+    fetch(url, {
+      method: "GET",
+    }).then((response) => {
+      if (response.ok) {
+        this.setState({ serverStatus: true, statusFailed: false });
+        console.log("Server connection established!");
+      } else {
+        this.setState({ statusFailed: true });
+      }
+    });
+  }
+
+  retryConnection() {
+    this.getServerStatus();
+  }
+
+  renderLoading() {
+    const { statusFailed, serverStatus } = this.state;
+    if (statusFailed) {
+      return <RetryWidget retryConnection={this.retryConnection.bind(this)} />;
+    }
+    return serverStatus ? null : <LoadingScreen />;
+  }
+
   render() {
-    return (
-      <div className="App">
-        <LoadingScreen></LoadingScreen>
-      </div>
-    );
+    return <div className="App">{this.renderLoading()}</div>;
   }
 }
 
